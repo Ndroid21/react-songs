c34fb4e3082af6bcd4dfe9dc3c33716434184e3e
programmingArrow <nishant.kashyap2104@gmail.com>
2019-12-15 23:08:59 +0530

    Added SongList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SongList from './SongList';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                Hello from App
+                <SongList />
             </div>
-        )
+        );
     }
 }
